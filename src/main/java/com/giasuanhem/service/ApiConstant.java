8614e1d2e070dcc6f171da52f3e62ade56907979
TrunNghia232 <trungnghiaazd@gmail,com>
2022-11-14 02:58:00 +0700

    no message

--- src/main/java/com/giasuanhem/service/ApiConstant.java
+++ src/main/java/com/giasuanhem/service/ApiConstant.java
@@ -7,4 +7,8 @@ public class ApiConstant{
 	public static final String LIST_CLASS = "/class/index";	
 	public static final String LIST_TUTOR = "/tutor/index";	
 	public static final String LIST_SUBJECT = "/subject/index";	
+	public static final String CLASS_REMOVE = "/class/remove";	
+	public static final String NEWCLAS_REMMOVE = "/new_class/remove";	
+	public static final String SUBJECT_REMOVE = "/subject/remove";
+	public static final String TUTOR_REMOVE = "/tutor/remove";
 }
