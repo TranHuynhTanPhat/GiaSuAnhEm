2b8b0ea7c6bb2bcf936d8df146da3d45dceda1f6
Trần Huỳnh Tấn Phát <tranhuynhtanphat9380@gmail.com>
2022-11-18 21:03:39 +0700

    no message

--- src/main/java/com/giasuanhem/controller/Client/HomeController.java
+++ src/main/java/com/giasuanhem/controller/Client/HomeController.java
@@ -22,35 +22,10 @@ import com.giasuanhem.service.Service.CommonService;
 public class HomeController {
 	@Autowired
 	private CommonService commonService;
-	
 
 	@RequestMapping(value = "/trang-chu", method = RequestMethod.GET)
-	public ModelAndView homePage() {
+	public ModelAndView homePage(HttpSession session) {
 
-		Map<String, Object> paramsClass = new HashMap<>();
-		paramsClass.put("style", 0);
-		List<CategoryModel> listClass = commonService.getListCategory(paramsClass);
-
-		Map<String, Object> paramsDistrict = new HashMap<>();
-		paramsDistrict.put("style", 1);
-		List<CategoryModel> listDistrict = commonService.getListCategory(paramsDistrict);
-
-		session.setAttribute("listCategoryClass", listClass);
-		session.setAttribute("listCategoryDistrict", listDistrict);
-		ModelAndView mav = new ModelAndView("home/home");
-//		mav.addObject("listCategoryClass", listClass);
-//		mav.addObject("listCategoryDistrict", listDistrict);
-		return mav;
-	}
-
-	@RequestMapping(value = "/springmvc", method = RequestMethod.GET)
-	public ModelAndView springMVCPage() {
-		ModelAndView mav = new ModelAndView("springmvc");
-		return mav;
-	}
-
-	@RequestMapping(value = "/gioi-thieu", method = RequestMethod.GET)
-	public ModelAndView instructionPage() {
 		Map<String, Object> paramsClass = new HashMap<>();
 		paramsClass.put("style", 0);
 		List<CategoryModel> listClass = commonService.getListCategory(paramsClass);
@@ -63,58 +38,45 @@ public class HomeController {
 		paramsIntroduction.put("style", 0);
 		List<PostModel> listPost = commonService.getListPostWithParams(paramsIntroduction);
 
+		session.setAttribute("listCategoryClass", listClass);
+		session.setAttribute("listCategoryDistrict", listDistrict);
+		session.setAttribute("listPost", listPost);
+
+		ModelAndView mav = new ModelAndView("home/home");
+		return mav;
+	}
+
+	@RequestMapping(value = "/springmvc", method = RequestMethod.GET)
+	public ModelAndView springMVCPage() {
+		ModelAndView mav = new ModelAndView("springmvc");
+		return mav;
+	}
+
+	@RequestMapping(value = "/gioi-thieu", method = RequestMethod.GET)
+	public ModelAndView instructionPage(HttpSession session) {
+
 		ModelAndView mav = new ModelAndView("home/introduce");
-		mav.addObject("listCategoryClass", listClass);
-		mav.addObject("listPost", listPost);
-		mav.addObject("listCategoryDistrict", listDistrict);
 		return mav;
 	}
 
 	@RequestMapping(value = "/tuyen-dung", method = RequestMethod.GET)
 	public ModelAndView recruitPage() {
-		Map<String, Object> paramsClass = new HashMap<>();
-		paramsClass.put("style", 0);
-		List<CategoryModel> listClass = commonService.getListCategory(paramsClass);
-
-		Map<String, Object> paramsDistrict = new HashMap<>();
-		paramsDistrict.put("style", 1);
-		List<CategoryModel> listDistrict = commonService.getListCategory(paramsDistrict);
 
 		ModelAndView mav = new ModelAndView("home/recruit");
-		mav.addObject("listCategoryClass", listClass);
-		mav.addObject("listCategoryDistrict", listDistrict);
 		return mav;
 	}
 
 	@RequestMapping(value = "muc-phi-gia-su", method = RequestMethod.GET)
 	public ModelAndView feeTutorPage() {
-		Map<String, Object> paramsClass = new HashMap<>();
-		paramsClass.put("style", 0);
-		List<CategoryModel> listClass = commonService.getListCategory(paramsClass);
-
-		Map<String, Object> paramsDistrict = new HashMap<>();
-		paramsDistrict.put("style", 1);
-		List<CategoryModel> listDistrict = commonService.getListCategory(paramsDistrict);
 
 		ModelAndView mav = new ModelAndView("home/feeTutor");
-		mav.addObject("listCategoryClass", listClass);
-		mav.addObject("listCategoryDistrict", listDistrict);
 		return mav;
 	}
 
 	@RequestMapping(value = "/lien-he", method = RequestMethod.GET)
 	public ModelAndView contactPage() {
-		Map<String, Object> paramsClass = new HashMap<>();
-		paramsClass.put("style", 0);
-		List<CategoryModel> listClass = commonService.getListCategory(paramsClass);
-
-		Map<String, Object> paramsDistrict = new HashMap<>();
-		paramsDistrict.put("style", 1);
-		List<CategoryModel> listDistrict = commonService.getListCategory(paramsDistrict);
 
 		ModelAndView mav = new ModelAndView("home/contact");
-		mav.addObject("listCategoryClass", listClass);
-		mav.addObject("listCategoryDistrict", listDistrict);
 		return mav;
 	}
 }
